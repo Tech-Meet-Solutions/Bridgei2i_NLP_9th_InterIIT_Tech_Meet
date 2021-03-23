610524d9a66e01bed2d06f30607e05b01c1a1ab9
Anshul Nasery <38972131+anshuln@users.noreply.github.com>
2021-03-23 18:14:22 +0530

    Adds final word list, removes fuzzy hindi matching

--- Task1/rule_based/rule_based.py
+++ Task1/rule_based/rule_based.py
@@ -9,11 +9,16 @@ import numpy as np
 from fuzzywuzzy import fuzz 
 from sklearn.metrics import classification_report
 
-
+# target_words = ['smartphone','स्मार्टफ़ोन','camera','android','स्मार्टफोन','earphones','फ़ोन','कैमरा','phone', 'samsung','xiaomi']
+target_words = ['smartphone','camera','earphones', 'samsung','xiaomi','iphone','blackberry','nokia','oneplus','mediatek','motorola']
+hindi_words = ['स्मार्टफ़ोन', 'स्मार्टफोन',  'कैमरा',]
 def brute_force_fuzzy(s1,s2):
 	# Returns the max match according to fuzzy matching
 	return max(fuzz.ratio(s1,s2), fuzz.partial_ratio(s1,s2), fuzz.token_sort_ratio(s1,s2),fuzz.token_set_ratio(s1,s2))
 
+
+def search_exact(word,string):
+	return int(any([word in c for c in string.split()]))
 def predict(x):
 	'''Predictor function
 	
@@ -25,9 +30,11 @@ def predict(x):
 	Returns:
 		float -- Probability of the tweet being relevant
 	'''
-	english_pred = brute_force_fuzzy("smartphone",x.lower())
-	hindi_pred = brute_force_fuzzy("स्मार्टफ़ोन",x)
-	return (max(hindi_pred,english_pred)>95)*1      # 95 here is a heuristic obtained on the train set!
+	pred = max([brute_force_fuzzy(word,x.lower()) for word in target_words])
+	pred_hind = max([search_exact(word,x) for word in hindi_words])
+	# english_pred = max(brute_force_fuzzy("smartphone",x.lower()),brute_force_fuzzy("device",x.lower()))
+	# hindi_pred = max(brute_force_fuzzy("स्मार्टफ़ोन",x),brute_force_fuzzy("स्मार्टफोन",x))
+	return max((pred>95)*1,pred_hind)      # 95 here is a heuristic obtained on the train set!
 
 
 def predict_exact(x):
@@ -37,10 +44,19 @@ def predict_exact(x):
 
 if __name__ == '__main__':
 	# Driver code to test
-	data = pd.read_csv('dev_data_tweet.csv')
-	y_pred = (data['Tweet'].apply(predict))
-	y_true = data['Mobile_Tech_Tag']
+	data = pd.read_csv('evaluation_data.csv')
+	print("lol")
+	print(data)
+	data['Flag'] = (data['Text'].apply(predict))
+	# y_true = data['Mobile_Tech_Flag']
+	# print(np.isinf(y_pred.any()),np.isinf(y_true.any()))
+	# col_mask=y_true.isnull()
+	# print(col_mask[col_mask==True].index)
+	# print((y_pred==y_true).sum())
+	# print(y_pred-y_true)
+	# print(classification_report(y_true,y_pred))
+	# print("These are the mis-classified IDs")
+	# idx = np.arange(4000)[y_true!=y_pred]
+	# print(idx)
 
-	print(classification_report(y_true,y_pred))
-	print("These are the mis-classified IDs")
-	print(np.arange(4000)[y_true!=y_pred])
+	data.to_csv('eval_preds_hindi.csv')
